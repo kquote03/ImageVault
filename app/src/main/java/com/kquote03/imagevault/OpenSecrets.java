e2a09ee4ac4cb07f7756cfc2d8c12418571439e7
kenan1099 <kquote03@gmail.com>
2023-04-16 02:02:15 +0400

    Added OpenSecrets class

---
+++ app/src/main/java/com/kquote03/imagevault/OpenSecrets.java
@@ -0,0 +1,38 @@
+package com.kquote03.imagevault;
+
+import javax.crypto.spec.IvParameterSpec;
+
+/**
+ * A class to store the IV and Salt of a file.
+ * Signed SSBsb3ZlIHlvdSwgWWhnaHUh
+ */
+public class OpenSecrets {
+    public IvParameterSpec iv;
+    public byte[] salt;
+
+    /**
+     * Constructor that takes in the IV and Salt
+     * @param iv
+     * @param salt
+     */
+    public OpenSecrets(IvParameterSpec iv, byte[] salt) {
+        this.iv = iv;
+        this.salt = salt;
+    }
+
+    public IvParameterSpec getIv() {
+        return iv;
+    }
+
+    public void setIv(IvParameterSpec iv) {
+        this.iv = iv;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+}
