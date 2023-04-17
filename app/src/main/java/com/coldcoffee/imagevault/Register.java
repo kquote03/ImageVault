8df204722b5f37692ca28ddeecab1f4b85baf574
kenan1099 <kquote03@gmail.com>
2023-04-17 20:15:00 +0400

    Registration actually fucking works now :D

--- app/src/main/java/com/coldcoffee/imagevault/Register.java
+++ app/src/main/java/com/coldcoffee/imagevault/Register.java
@@ -10,20 +10,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 public class Register extends AppCompatActivity {
     EditText username;
-
     TextView logintext;
     EditText Email;
     EditText password1;
     EditText password2;
     Button signupButton;
+    AppDB db;
+    CryptoUtils crypto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        db = Room.databaseBuilder(getApplicationContext(),
+                AppDB.class, "user").allowMainThreadQueries().build();
+
+        crypto = new CryptoUtils(getApplicationContext());
+
         username = findViewById(R.id.username);
         Email = findViewById(R.id.Email);
         password1 = findViewById(R.id.Password1);
@@ -37,6 +45,14 @@ public class Register extends AppCompatActivity {
                         || Email.getText().toString().equals("") || Email.getText().equals(null)
                         || password1.getText().toString().equals("") ||  password1.getText().equals(null))
                     Toast.makeText(Register.this, "Failed to sign up", Toast.LENGTH_LONG).show();
+                else{
+                    db.userDao().insertUser(new User(username.getText().toString(),crypto.generateIv().getIV(),crypto.generateSalt()));
+                    try {
+                        crypto.storeSecretKey(username.getText().toString(), crypto.getKeyFromPassword(password1.getText().toString(),db.userDao().getSalt(username.getText().toString())), password1.getText().toString());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
 
             }
         });
