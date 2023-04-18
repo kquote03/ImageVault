27ff836e8400af42558c939c0c9f0790b926db64
kenan1099 <kquote03@gmail.com>
2023-04-18 05:43:12 +0400

    Poor excuse of a splash screen

--- app/src/main/java/com/coldcoffee/imagevault/MainActivity.java
+++ app/src/main/java/com/coldcoffee/imagevault/MainActivity.java
@@ -1,4 +1,17 @@
 package com.coldcoffee.imagevault;
 
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
 }
